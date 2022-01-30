5600dcedb2fa9277989d40e09daf2b4f256412e0
Erik Mikac <mikacew@gmail.com>
2022-01-30 13:56:36 -0500

    Initial Commit

---
+++ MadLib1/MadLib1App.swift
@@ -0,0 +1,17 @@
+//
+//  MadLib1App.swift
+//  MadLib1
+//
+//  Created by Erik Mikac on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MadLib1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
